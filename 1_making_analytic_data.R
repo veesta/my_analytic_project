b05248e9aa50f9141fc69300a2ec2a6f7549e8d2
veesta <vmavanda@uoguelph.ca>
2016-09-20 13:12:44 -0400

    made factors and scales

--- 1_making_analytic_data.R
+++ 1_making_analytic_data.R
@@ -6,6 +6,20 @@ raw_data <-read_csv(file="rawData.csv")
 
 str(raw_data)
 
+##how to view the table
 #View(raw_data)
 
+##add NA instead of numbers
 raw_data <-read_csv(file="rawData.csv" ,na=c("","NA","-999","-888"))
+
+#View(raw_data)
+
+#create data with just categorical variables
+categorical_variables <- select(raw_data, group, gender)
+
+#tell R variables are categorical
+categorical_variables$group <- as.factor(categorical_variables$group)
+
+#assign labels to category
+categorical_variables$gender <-as.factor(categorical_variables$gender)
+levels(categorical_variables$gender) <- list("Male"=1, "Female"=2)
